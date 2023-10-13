3a51d78d80419cb39cde9bc8b987642413512b61
MD-Hasibul_Hasan <hasibulofcs@gmail.com>
2023-10-13 15:19:22 +0600

    updated blog section. Several fixes

---
+++ src/data/hero/HeroData.js
@@ -0,0 +1,24 @@
+const contents = [
+  {
+    text: "Web Design",
+    link: "/",
+  },
+  {
+    text: "UI/UX design",
+    link: "/",
+  },
+  {
+    text: "Web3",
+    link: "/",
+  },
+  {
+    text: "Content writing",
+    link: "/",
+  },
+  {
+    text: "Branding",
+    link: "/",
+  },
+];
+
+export default contents;
